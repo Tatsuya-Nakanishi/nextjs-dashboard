3ce2acdef1c21236aa599c7e0acf5c589b00aa42
tatsuya-nakanishi <jwtja6wv@gmail.com>
2024-11-28 18:56:56 +0900

    チャプター５まで

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana as LusitanaFont } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const Lusitana = LusitanaFont({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
